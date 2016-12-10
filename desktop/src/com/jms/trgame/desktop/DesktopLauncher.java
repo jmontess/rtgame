7ae6bd3e74c8ddd4d5834058a12b7bf33232751f
Jesus Montes <jesus.montes@gmail.com>
2016-12-10 12:00:24 +0100

    Cleaning code in desktop launcher.

--- desktop/src/com/jms/trgame/desktop/DesktopLauncher.java
+++ desktop/src/com/jms/trgame/desktop/DesktopLauncher.java
@@ -6,12 +6,10 @@ import com.jms.trgame.TRGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = TRGame.GAME_TITLE;
-		//config.x = TRGame.SCREEN_WIDTH;
-		//config.y = TRGame.SCREEN_HEIGHT;
-        config.width = TRGame.SCREEN_WIDTH;
-        config.height = TRGame.SCREEN_HEIGHT;
-		new LwjglApplication(new TRGame(), config);
+	    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	    config.title = TRGame.GAME_TITLE;
+	    config.width = TRGame.SCREEN_WIDTH;
+	    config.height = TRGame.SCREEN_HEIGHT;
+	    new LwjglApplication(new TRGame(), config);
 	}
 }
